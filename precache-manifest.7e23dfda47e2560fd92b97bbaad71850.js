a51a8af01c2fed558441ac3add79d0781a61ac9f
Filip <filip.k.wanat@gmail.com>
2020-03-04 19:13:48 +0100

    Updates

---
+++ precache-manifest.7e23dfda47e2560fd92b97bbaad71850.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b40c3efbf095b9da35bd0966e6b1e95",
+    "url": "/todo-intro/index.html"
+  },
+  {
+    "revision": "3370213d44a19f508ee3",
+    "url": "/todo-intro/static/css/2.485abd6b.chunk.css"
+  },
+  {
+    "revision": "6e45cbee05be8683fbf4",
+    "url": "/todo-intro/static/css/main.c10cb32f.chunk.css"
+  },
+  {
+    "revision": "3370213d44a19f508ee3",
+    "url": "/todo-intro/static/js/2.bbfa010c.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/todo-intro/static/js/2.bbfa010c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e45cbee05be8683fbf4",
+    "url": "/todo-intro/static/js/main.e02adbf3.chunk.js"
+  },
+  {
+    "revision": "220372d45be64efe34a1",
+    "url": "/todo-intro/static/js/runtime-main.62d6f422.js"
+  }
+]);
