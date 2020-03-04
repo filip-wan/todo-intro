79c0211c5417e188f1fa2c51d5bd4952637b108f
Filip <filip.k.wanat@gmail.com>
2020-03-04 06:13:48 +0100

    Remove biolerplate layout and add themed bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import './styles/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootswatch/dist/darkly/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
